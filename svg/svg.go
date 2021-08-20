34b55570253f278f94a98729b8c168f529947c86
8i8 <iain.coding@gmail.com>
2021-08-20 15:11:35 +0200

    running setup changed

--- svg/svg.go
+++ svg/svg.go
@@ -3,19 +3,20 @@ package svg
 import (
 	"fmt"
 	"io"
+	"svg/svg/lex"
 
 	"github.com/8i8/conf"
 )
 
 func Default(in io.ReadCloser, c *conf.Config) {
 	root := Parse(in)
-	// l := lex.NewLexer()
-	// defer l.Close()
-	//err := setAttrStyle(l, root, "flame", replace{"style", "stroke", "blue"})
+	l := lex.NewLexer()
+	defer l.Close()
+	err := setAttrStyle(l, root, "flame", replace{"style", "stroke", "blue"})
 	//err := setAttrStyle(l, root, "words", replace{"style", "font-family", "monospace"})
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	if err != nil {
+		fmt.Println(err)
+	}
 	pprint(root)
 }
 
